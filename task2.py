3e6ef8f62d1a0b7f2533b17100f0fc0253f12726
saisahithikotte2603-arch <saisahithikotte2603@gmail.com>
2026-02-16 20:27:58 +0530

    Add files via upload
    Assignment2

--- task2.py
+++ task2.py
@@ -1,4 +1,4 @@
-first_name=input("Enter your first name:")
-last_name=input("Enter your last name:")
-full_name= first_name + " " + last_name
-print("Hello, " + full_name + "! Welcome to Python programming.")
+total_sum=0
+for i in range(1,51):
+    total_sum+=i
+print("The sum of numbers from 1 to 50 is:",total_sum)
